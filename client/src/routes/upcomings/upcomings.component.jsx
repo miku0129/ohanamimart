fef83724228c889edc23b128f17e97aeba814d18
miku0129 <svartkatt39@gmail.com>
2023-07-26 18:17:59 +0200

    applied position:sticky to navigation

--- client/src/routes/upcomings/upcomings.component.jsx
+++ client/src/routes/upcomings/upcomings.component.jsx
@@ -1,7 +1,20 @@
 import "./upcomings.styles.scss";
 
 const Upcomings = () => {
-  return <h1>up comings</h1>;
+  return (
+    <div>
+      <div className="image-container">
+        <img
+          src="https://i.ibb.co/S3dBd36/360051126-1297912624141236-3022852886206423713-n.jpg"
+          alt="event_image"
+        />
+      </div>
+      <div className="contents-container">
+        <div className="contents1"></div>
+        <div className="contents2"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Upcomings;
