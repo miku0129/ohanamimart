c45197a301f9ac9b5b974d924e49c900600a52f8
miku0129 <svartkatt39@gmail.com>
2023-07-27 16:21:52 +0200

    add styles to modal

--- client/src/compoments/exhibition-preview/exhibition-preview.component.jsx
+++ client/src/compoments/exhibition-preview/exhibition-preview.component.jsx
@@ -7,27 +7,40 @@ const ExhibitionPreview = ({ exhibitionMap }) => {
 
   return (
     exhibitionMap && (
-      <div className="contents-container">
-        <div className="image-container">
-          <img src={imageUrl} alt={exhibitionTitle} />
-        </div>
-        <div className="contents-subcontainer">
-          <div className="contents-subcontainer-left">
-            <h2>{exhibitionTitle}</h2>
-            <span>{date}</span>
-            <br />
-            <span>{time}</span>
-            <hr />
-            <span>{place}</span>
-            <br />
-            <span>{address}</span>
-            <hr />
+      <div>
+        <div className="contents-container">
+          <div className="image-container">
+            <img src={imageUrl} alt={exhibitionTitle} />
           </div>
-          <div className="contents-subcontainer-right">
-            <h2>Exhibitors</h2>
-            { exhibitors && exhibitors.map((exhibitor) => {
-              return <ExhibitorCard key={exhibitor.id} exhibitor={exhibitor} />;
-            })}
+          <div className="contents-subcontainer">
+            <div className="contents-subcontainer-left">
+              <h2>{exhibitionTitle}</h2>
+              <span>{date}</span>
+              <br />
+              <span>{time}</span>
+              <hr />
+              <span>{place}</span>
+              <br />
+              <span>{address}</span>
+              <hr />
+            </div>
+            <div className="contents-subcontainer-right">
+              <h2>Exhibitors</h2>
+              {exhibitors &&
+                exhibitors.map((exhibitor) => {
+                  return (
+                    <ExhibitorCard key={exhibitor.id} exhibitor={exhibitor} />
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+        <div id="overlay">
+          <div className="modal-image-container">
+            <div className="modal-image-subcontainer">
+              <button>&times;</button>
+            </div>
+            <img src={imageUrl} alt={exhibitionTitle} />
           </div>
         </div>
       </div>
