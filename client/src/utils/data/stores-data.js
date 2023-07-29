4a1093773da22bc43828d36b10113cb3152748a1
miku0129 <svartkatt39@gmail.com>
2023-07-29 15:20:59 +0200

    modefied labels of product in product-card.component.jsx

--- client/src/utils/data/stores-data.js
+++ client/src/utils/data/stores-data.js
@@ -11,57 +11,57 @@ const STORES_DATA = {
       products: [
         {
           id: 1,
-          name: "Bag",
-          imageUrl:
+          product_name: "Bag",
+          product_image_url:
             "https://i.ibb.co/mDJbMXH/340156543-1580592949073736-1963966559106618963-n.jpg",
-          price: 25,
+          product_price: 25,
         },
         {
           id: 2,
-          name: "Bag",
-          imageUrl:
+          product_name: "Bag",
+          product_image_url:
             "https://i.ibb.co/H2qq14s/339507433-988881402522640-2439768866332386619-n.jpg",
-          price: 18,
+          product_price: 18,
         },
         {
           id: 3,
-          name: "Bag",
-          imageUrl:
+          product_name: "Bag",
+          product_image_url:
             "https://i.ibb.co/18bQpcy/315111862-202827022207293-6951652126240759731-n.jpg",
-          price: 35,
+          product_price: 35,
         },
         {
           id: 4,
-          name: "Bag",
-          imageUrl:
+          product_name: "Bag",
+          product_image_url:
             "https://i.ibb.co/NFb1szM/307029019-1726574081037759-4093308280242153544-n.jpg",
-          price: 25,
+          product_price: 25,
         },
         {
           id: 5,
-          name: "Bag",
-          imageUrl:
+          product_name: "Bag",
+          product_image_url:
             "https://i.ibb.co/Fx4PHtL/310511217-505942311035532-6580455511301699967-n.jpg",
-          price: 18,
+          product_price: 18,
         },
         {
           id: 6,
-          name: "Bag",
-          imageUrl: "https://i.ibb.co/BCKtdTV/tubakicr-ation2.jpg",
-          price: 14,
+          product_name: "Bag",
+          product_image_url: "https://i.ibb.co/BCKtdTV/tubakicr-ation2.jpg",
+          product_price: 14,
         },
         {
           id: 7,
-          name: "Bag",
-          imageUrl:
+          product_name: "Bag",
+          product_image_url:
             "https://i.ibb.co/tK1YGN6/272019748-321633993212231-2159083599050553166-n.jpg",
-          price: 18,
+          product_price: 18,
         },
         {
           id: 8,
-          name: "Bag",
-          imageUrl: "https://i.ibb.co/mqnn9Zw/tubakicr-ation.jpg",
-          price: 14,
+          product_name: "Bag",
+          product_image_url: "https://i.ibb.co/mqnn9Zw/tubakicr-ation.jpg",
+          product_price: 14,
         },
       ],
     },
@@ -75,58 +75,58 @@ const STORES_DATA = {
       products: [
         {
           id: 10,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/qk9D5WC/346751694-805189864354339-6667077890498622125-n.jpg",
-          price: 220,
+          product_price: 220,
         },
         {
           id: 11,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/TRnZ0Zx/311128417-633392271648201-7039921346873529879-n.jpg",
-          price: 280,
+          product_price: 280,
         },
         {
           id: 12,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/wYmM3P7/310458558-197204246005009-4038288699559648048-n.jpg",
-          price: 110,
+          product_price: 110,
         },
         {
           id: 13,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/XznqDZM/286512302-1822551324754596-5747116266699561890-n.jpg",
-          price: 160,
+          product_price: 160,
         },
         {
           id: 14,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/CQHmynf/332850510-732754068541099-3346283014744796636-n.jpg",
-          price: 160,
+          product_price: 160,
         },
         {
           id: 15,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/5rvBBxQ/339978302-3193268257629935-2675262353004250603-n.jpg",
-          price: 160,
+          product_price: 160,
         },
         {
           id: 16,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/CmWWxZq/344597211-5538146432952014-2015759206251185477-n.jpg",
-          price: 190,
+          product_price: 190,
         },
         {
           id: 17,
-          name: "Accessory",
-          imageUrl: "https://i.ibb.co/x7BmYY8/at-bijoux.jpg",
-          price: 200,
+          product_name: "Accessory",
+          product_image_url: "https://i.ibb.co/x7BmYY8/at-bijoux.jpg",
+          product_price: 200,
         },
       ],
     },
@@ -140,58 +140,58 @@ const STORES_DATA = {
       products: [
         {
           id: 18,
-          name: "Manga",
-          imageUrl:
+          product_name: "Manga",
+          product_image_url:
             "https://i.ibb.co/VwJqBFK/12106068-775724685872346-1545133245-n.jpg",
-          price: 125,
+          product_price: 125,
         },
         {
           id: 19,
-          name: "Manga",
-          imageUrl:
+          product_name: "Manga",
+          product_image_url:
             "https://i.ibb.co/KGD7F0y/12093370-152164235131907-1723586096-n.jpg",
-          price: 90,
+          product_price: 90,
         },
         {
           id: 20,
-          name: "Manga",
-          imageUrl:
+          product_name: "Manga",
+          product_image_url:
             "https://i.ibb.co/zPzYq8M/12145210-165338877143967-1583655975-n.jpg",
-          price: 90,
+          product_price: 90,
         },
         {
           id: 21,
-          name: "Art",
-          imageUrl:
+          product_name: "Art",
+          product_image_url:
             "https://i.ibb.co/1GkC4X7/12331552-1685794798305212-973138596-n.jpg",
-          price: 165,
+          product_price: 165,
         },
         {
           id: 22,
-          name: "Art",
-          imageUrl:
+          product_name: "Art",
+          product_image_url:
             "https://i.ibb.co/K0sHr7k/12534423-857146337741021-54165594-n.jpg",
-          price: 185,
+          product_price: 185,
         },
         {
           id: 221,
-          name: "Art",
-          imageUrl:
+          product_name: "Art",
+          product_image_url:
             "https://i.ibb.co/PCwbZnn/182425265-143462637753408-656123107402387604-n.jpg",
-          price: 185,
+          product_price: 185,
         },
         {
           id: 222,
-          name: "Art",
-          imageUrl:
+          product_name: "Art",
+          product_image_url:
             "https://i.ibb.co/28ZpSJX/183173658-302266474606695-3469464846294373328-n.jpg",
-          price: 185,
+          product_price: 185,
         },
         {
           id: 223,
-          name: "Art",
-          imageUrl: "https://i.ibb.co/545P4Zb/namicr-ations.jpg",
-          price: 185,
+          product_name: "Art",
+          product_image_url: "https://i.ibb.co/545P4Zb/namicr-ations.jpg",
+          product_price: 185,
         },
       ],
     },
@@ -205,58 +205,58 @@ const STORES_DATA = {
       products: [
         {
           id: 23,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/xHtcZKt/350796599-1460853344452013-7446909390185489329-n.jpg",
-          price: 25,
+          product_price: 25,
         },
         {
           id: 24,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/8Y4ypgZ/286909839-593581938632081-240851648592919563-n.jpg",
-          price: 20,
+          product_price: 20,
         },
         {
           id: 25,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/j443z8K/350569496-1281611739443222-1522847207843220052-n.jpg",
-          price: 80,
+          product_price: 80,
         },
         {
           id: 26,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/YyJzxf1/272898910-3243367145893928-8824315507361064424-n.jpg",
-          price: 80,
+          product_price: 80,
         },
         {
           id: 27,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/KwffpvH/318753451-205576898579698-6439655907164669935-n.jpg",
-          price: 45,
+          product_price: 45,
         },
         {
           id: 28,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/C9v1436/339986387-236553878857231-4716027452308966452-n.jpg",
-          price: 135,
+          product_price: 135,
         },
         {
           id: 29,
-          name: "Accessory",
-          imageUrl: "https://i.ibb.co/9nFGMb9/goen.jpg",
-          price: 20,
+          product_name: "Accessory",
+          product_image_url: "https://i.ibb.co/9nFGMb9/goen.jpg",
+          product_price: 20,
         },
         {
           id: 30,
-          name: "Accessory",
-          imageUrl:
+          product_name: "Accessory",
+          product_image_url:
             "https://i.ibb.co/NZX9kj0/269841067-3214165605486600-1323362896622091483-n.jpg",
-          price: 20,
+          product_price: 20,
         },
       ],
     },
