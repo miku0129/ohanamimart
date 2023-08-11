80773f690eae4837011ce1c30f71ce6ff5acac5c
miku0129 <svartkatt39@gmail.com>
2023-08-11 16:41:27 +0200

    remove dummy price from shops-data

--- client/src/utils/data/shops-data.js
+++ client/src/utils/data/shops-data.js
@@ -17,54 +17,54 @@ const SHOPS_DATA = {
           product_name: "Bag",
           product_image_url:
             "https://i.ibb.co/mDJbMXH/340156543-1580592949073736-1963966559106618963-n.jpg",
-          product_price: 25,
+          product_price: null,
         },
         {
           id: 2,
           product_name: "Bag",
           product_image_url:
             "https://i.ibb.co/H2qq14s/339507433-988881402522640-2439768866332386619-n.jpg",
-          product_price: 18,
+          product_price: null,
         },
         {
           id: 3,
           product_name: "Bag",
           product_image_url:
             "https://i.ibb.co/18bQpcy/315111862-202827022207293-6951652126240759731-n.jpg",
-          product_price: 35,
+          product_price: null,
         },
         {
           id: 4,
           product_name: "Bag",
           product_image_url:
             "https://i.ibb.co/NFb1szM/307029019-1726574081037759-4093308280242153544-n.jpg",
-          product_price: 25,
+          product_price: null,
         },
         {
           id: 5,
           product_name: "Bag",
           product_image_url:
             "https://i.ibb.co/Fx4PHtL/310511217-505942311035532-6580455511301699967-n.jpg",
-          product_price: 18,
+          product_price: null,
         },
         {
           id: 6,
           product_name: "Bag",
           product_image_url: "https://i.ibb.co/BCKtdTV/tubakicr-ation2.jpg",
-          product_price: 14,
+          product_price: null,
         },
         {
           id: 7,
           product_name: "Bag",
           product_image_url:
             "https://i.ibb.co/tK1YGN6/272019748-321633993212231-2159083599050553166-n.jpg",
-          product_price: 18,
+          product_price: null,
         },
         {
           id: 8,
           product_name: "Bag",
           product_image_url: "https://i.ibb.co/mqnn9Zw/tubakicr-ation.jpg",
-          product_price: 14,
+          product_price: null,
         },
       ],
     },
@@ -156,55 +156,55 @@ const SHOPS_DATA = {
           product_name: "Manga",
           product_image_url:
             "https://i.ibb.co/VwJqBFK/12106068-775724685872346-1545133245-n.jpg",
-          product_price: 125,
+          product_price: null,
         },
         {
           id: 19,
           product_name: "Manga",
           product_image_url:
             "https://i.ibb.co/KGD7F0y/12093370-152164235131907-1723586096-n.jpg",
-          product_price: 90,
+          product_price: null,
         },
         {
           id: 20,
           product_name: "Manga",
           product_image_url:
             "https://i.ibb.co/zPzYq8M/12145210-165338877143967-1583655975-n.jpg",
-          product_price: 90,
+          product_price: null,
         },
         {
           id: 21,
           product_name: "Art",
           product_image_url:
             "https://i.ibb.co/1GkC4X7/12331552-1685794798305212-973138596-n.jpg",
-          product_price: 165,
+          product_price: null,
         },
         {
           id: 22,
           product_name: "Art",
           product_image_url:
             "https://i.ibb.co/K0sHr7k/12534423-857146337741021-54165594-n.jpg",
-          product_price: 185,
+          product_price: null,
         },
         {
           id: 221,
           product_name: "Art",
           product_image_url:
             "https://i.ibb.co/PCwbZnn/182425265-143462637753408-656123107402387604-n.jpg",
-          product_price: 185,
+          product_price: null,
         },
         {
           id: 222,
           product_name: "Art",
           product_image_url:
             "https://i.ibb.co/28ZpSJX/183173658-302266474606695-3469464846294373328-n.jpg",
-          product_price: 185,
+          product_price: null,
         },
         {
           id: 223,
           product_name: "Art",
           product_image_url: "https://i.ibb.co/545P4Zb/namicr-ations.jpg",
-          product_price: 185,
+          product_price: null,
         },
       ],
     },
@@ -223,55 +223,55 @@ const SHOPS_DATA = {
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/xHtcZKt/350796599-1460853344452013-7446909390185489329-n.jpg",
-          product_price: 25,
+          product_price: null,
         },
         {
           id: 24,
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/8Y4ypgZ/286909839-593581938632081-240851648592919563-n.jpg",
-          product_price: 20,
+          product_price: null,
         },
         {
           id: 25,
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/j443z8K/350569496-1281611739443222-1522847207843220052-n.jpg",
-          product_price: 80,
+          product_price: null,
         },
         {
           id: 26,
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/YyJzxf1/272898910-3243367145893928-8824315507361064424-n.jpg",
-          product_price: 80,
+          product_price: null,
         },
         {
           id: 27,
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/KwffpvH/318753451-205576898579698-6439655907164669935-n.jpg",
-          product_price: 45,
+          product_price: null,
         },
         {
           id: 28,
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/C9v1436/339986387-236553878857231-4716027452308966452-n.jpg",
-          product_price: 135,
+          product_price: null,
         },
         {
           id: 29,
           product_name: "Accessory",
           product_image_url: "https://i.ibb.co/9nFGMb9/goen.jpg",
-          product_price: 20,
+          product_price: null,
         },
         {
           id: 30,
           product_name: "Accessory",
           product_image_url:
             "https://i.ibb.co/NZX9kj0/269841067-3214165605486600-1323362896622091483-n.jpg",
-          product_price: 20,
+          product_price: null,
         },
       ],
     },
