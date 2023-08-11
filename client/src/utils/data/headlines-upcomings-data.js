6e436873b3b0e2d6278dac43e7617fb45ba9eac2
miku0129 <svartkatt39@gmail.com>
2023-08-11 17:11:13 +0200

    updated images in home directory

--- client/src/utils/data/headlines-upcomings-data.js
+++ client/src/utils/data/headlines-upcomings-data.js
@@ -3,7 +3,7 @@ const UPCOMINGS = {
   title: "Up comings",
   url_title_lowercase_no_spaces: "upcomings",
   image_url:
-    "https://images.unsplash.com/photo-1590422886897-7dd50e58577e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+    "https://i.ibb.co/jVfthGC/julia-caesar-m-Ea-YSkuc-Tys-unsplash.jpg",
 };
 
 export default UPCOMINGS;
