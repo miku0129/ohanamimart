275a72f414a1f3ed5f8d0e4bdb14c56385786957
miku0129 <svartkatt39@gmail.com>
2024-03-03 20:35:05 +0100

    docs: add envent for 2024/3/9

--- client/src/assets/main-visual-images.js
+++ client/src/assets/main-visual-images.js
@@ -1,38 +1,38 @@
 const MainVisualImages = [
   {
     id: 0,
+    src: "https://i.ibb.co/kQBps9Y/28455.jpg",
+    alt: "Image 6",
+  },
+  {
+    id: 1,
     src: "https://i.ibb.co/xhWvYWW/28340.jpg",
     alt: "Flyer of upcoming event",
   },
   {
-    id: 1,
+    id: 2,
     src: "https://i.ibb.co/QvFvydy/394973053-983669776025560-9208465077164965767-n.jpg",
     alt: "Image 1",
   },
   {
-    id: 2,
+    id: 3,
     src: "https://i.ibb.co/3kbt3Db/colabo-1.jpg",
     alt: "Image 2",
   },
   {
-    id: 3,
+    id: 4,
     src: "https://i.ibb.co/q9q3ZW3/colabo-3.jpg",
     alt: "Image 3",
   },
   {
-    id: 4,
+    id: 5,
     src: "https://i.ibb.co/0fy1vSP/colago-2.jpg",
     alt: "Image 4",
   },
   {
-    id: 5,
+    id: 6,
     src: "https://i.ibb.co/LZ9bqT8/Yuki-sweets-3.jpg",
     alt: "Image 5",
-  },
-  {
-    id: 6,
-    src: "https://i.ibb.co/3rL2ncg/marche.jpg",
-    alt: "Image 6",
   },
 ];
 
