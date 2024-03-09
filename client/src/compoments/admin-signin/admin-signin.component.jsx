fdce85e2f6b3ed89aace7e5115358fcf9b856481
miku0129 <svartkatt39@gmail.com>
2024-03-09 14:19:46 +0100

    update githubaction config

--- client/src/compoments/admin-signin/admin-signin.component.jsx
+++ client/src/compoments/admin-signin/admin-signin.component.jsx
@@ -1,6 +1,9 @@
 import HankoAuth from "../../hanko/hanko-auth/hanko-auth.component"
 
 const AdminSignin = () => {
+  const hankoApi = process.env.REACT_APP_HANKO_API_URL;
+console.log("??", hankoApi)
+
   return (
     <div>
       <HankoAuth />
