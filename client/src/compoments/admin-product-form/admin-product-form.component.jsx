ef57833987ffa8c2f1f45ada2f651d5dfec2a060
miku0129 <svartkatt39@gmail.com>
2024-03-09 12:01:15 +0100

    feat:add validation

--- client/src/compoments/admin-product-form/admin-product-form.component.jsx
+++ client/src/compoments/admin-product-form/admin-product-form.component.jsx
@@ -9,11 +9,13 @@ import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { CustomContentContainer } from "../../component-utils/component-utils.styles";
-import "./admin-product-form.styles.scss"
+import "./admin-product-form.styles.scss";
 
 const AdminProductForm = ({ props }) => {
   const { formType, shopId, initFormState, product_id } = props;
   const [formData, setFormData] = useState(initFormState);
+  const [validated, setValidated] = useState(false);
+
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
@@ -21,10 +23,22 @@ const AdminProductForm = ({ props }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      window.alert("入力データが正しくありません");
+      return;
+    }
+    setValidated(true);
+
     const product = {
       product_name: formData.product_name,
       product_description: formData.product_description,
-      product_price: Number(formData.product_price),
+      product_price:
+        typeof formData.product_price === "undefined"
+          ? null
+          : Number(formData.product_price),
       shop_id: shopId,
       is_product_image_used_in_main_visual: false,
       is_book: false,
@@ -45,36 +59,48 @@ const AdminProductForm = ({ props }) => {
 
   return (
     <CustomContentContainer className="admin-product-form">
-      <Form >
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
         <Row>
-          <Form.Label htmlFor="product_name">アイテムの名前</Form.Label>
-          <Form.Control
-            type="text"
-            id="product_name"
-            name="product_name"
-            value={formData.product_name}
-            onChange={handleChange}
-          />
-          <Form.Label htmlFor="product_price">
-            アイテムの価格(単位/euro)
-          </Form.Label>
-          <Form.Control
-            type="text"
-            id="product_price"
-            name="product_price"
-            value={formData.product_price}
-            onChange={handleChange}
-          />
-          <Form.Label htmlFor="product_description">アイテムの説明</Form.Label>
-          <Form.Control
-            type="text"
-            id="product_description"
-            name="product_description"
-            value={formData.product_description}
-            onChange={handleChange}
-          />
+          <Form.Group>
+            <Form.Label htmlFor="product_name">アイテムの名前</Form.Label>
+            <Form.Control
+              type="text"
+              id="product_name"
+              name="product_name"
+              value={formData.product_name}
+              onChange={handleChange}
+              required
+            />
+            <Form.Control.Feedback type="invalid">
+              アイテムの名前は必須です
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="product_price">
+              アイテムの価格(単位/euro)
+            </Form.Label>
+            <Form.Control
+              type="number"
+              id="product_price"
+              name="product_price"
+              value={formData.product_price}
+              onChange={handleChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label htmlFor="product_description">
+              アイテムの説明
+            </Form.Label>
+            <Form.Control
+              type="text"
+              id="product_description"
+              name="product_description"
+              value={formData.product_description}
+              onChange={handleChange}
+            />
+          </Form.Group>
           {formType === formTypes["UPDATE"] && formData && (
-            <div>
+            <Form.Group>
               <Form.Label htmlFor="product_images">アイテム写真URL</Form.Label>
               <Form.Control
                 type="text"
@@ -85,11 +111,15 @@ const AdminProductForm = ({ props }) => {
                   formData.product_images[0].product_image_url
                 }
                 onChange={handleChange}
+                required
               />
-            </div>
+              <Form.Control.Feedback type="invalid">
+                アイテム写真のURLは必須です
+              </Form.Control.Feedback>
+            </Form.Group>
           )}
           {formType === formTypes["REGISTER"] && (
-            <div>
+            <Form.Group>
               <Form.Label htmlFor="product_images">アイテム写真URL</Form.Label>
               <Form.Control
                 type="text"
@@ -97,12 +127,16 @@ const AdminProductForm = ({ props }) => {
                 name="product_images"
                 value={formData.product_images}
                 onChange={handleChange}
+                required
               />
-            </div>
+              <Form.Control.Feedback type="invalid">
+                アイテム写真のURLは必須です
+              </Form.Control.Feedback>
+            </Form.Group>
           )}
         </Row>
         <br />
-        <Button variant="success" type="submit" onClick={handleSubmit}>
+        <Button variant="success" type="submit">
           {formType === formTypes["REGISTER"]
             ? "アイテムを追加する"
             : "アイテムを更新する"}
