026198c6629b0d478e5309d3eaa4562f0d2db377
miku0129 <svartkatt39@gmail.com>
2024-03-26 18:19:40 +0100

    style: replace pictures in mainvisual with resized pictures

--- client/src/assets/main-visual-images.js
+++ client/src/assets/main-visual-images.js
@@ -6,52 +6,52 @@ const MainVisualImages = [
   },
   {
     id: 1,
-    src: "https://i.ibb.co/VLX9K0S/nami2JPG.png",
+    src: "https://i.ibb.co/LkC3QNz/akane3-resize.png",
     alt: "item",
   },
   {
     id: 2,
-    src: "https://i.ibb.co/Vvbh6qZ/nami1.png",
+    src: "https://i.ibb.co/CQLSVV3/akane-resize.png",
     alt: "item",
   },
   {
     id: 3,
-    src: "https://i.ibb.co/qr8x9Dh/nami3.png",
+    src: "https://i.ibb.co/QYM0rTB/hiro3-resize.png",
     alt: "item",
   },
   {
     id: 4,
-    src: "https://i.ibb.co/74RZ7L0/naomi.png",
+    src: "https://i.ibb.co/w0KqtSy/hiro1-resize.png",
     alt: "item",
   },
   {
     id: 5,
-    src: "https://i.ibb.co/0rPYLgd/akane.png",
+    src: "https://i.ibb.co/Gdzvfgz/naomi-resize.png",
     alt: "item",
   },
   {
     id: 6,
-    src: "https://i.ibb.co/t8G0RyM/akane2-2.png",
+    src: "https://i.ibb.co/YQmR5HY/nami3-resize.png",
     alt: "item",
   },
   {
     id: 7,
-    src: "https://i.ibb.co/SKFNj56/hiro3.png",
+    src: "https://i.ibb.co/RT6VbDp/nami2-resize.png",
     alt: "item",
   },
   {
     id: 8,
-    src: "https://i.ibb.co/PFHgz2W/hiro4-3.png",
+    src: "https://i.ibb.co/8mGDkRm/nami1-resize.png",
     alt: "item",
   },
   {
     id: 0,
-    src: "https://i.ibb.co/dKKDfCM/akane3.png",
+    src: "https://i.ibb.co/PFHgz2W/hiro4-3.png",
     alt: "item",
   },
   {
     id: 10,
-    src: "https://i.ibb.co/52p9MQ6/hiro1.png",
+    src: "https://i.ibb.co/t8G0RyM/akane2-2.png",
     alt: "item",
   },
 
