903a244270b19b5ab313bd039b67e493d33417da
miku0129 <svartkatt39@gmail.com>
2024-03-26 17:33:36 +0100

    feat: remove old functions of data-initialization

--- client/src/utils/firebase/firebase.helper.js
+++ client/src/utils/firebase/firebase.helper.js
@@ -19,31 +19,6 @@ export const makeArrayOfImagesForTheProduct = (
   return images_array;
 };
 
-//firesotre, shopsで使用
-// export const makeProductsArray_for_initializeCategoryData = (
-//   index,
-//   product_data,
-//   product_image_data
-// ) => {
-//   const { products } = product_data;
-//   const { product_images } = product_image_data;
-
-//   let products_array =
-//     products.length > 0
-//       ? products.filter((product) => product.shop_id === index)
-//       : null;
-//   if (products_array !== null) {
-//     products_array = products_array.map((product) => {
-//       const images = product_images.filter(
-//         (image) => image.product_id === product.id
-//       );
-//       const result = { ...product, product_images: images };
-//       return result;
-//     });
-//   }
-//   return products_array;
-// };
-
 //FireStoreにおいてデータは必ずしも追加順に格納されないため
 //最後尾のIDを取得するためにソートする必要がある。
 export const getTheTailendId = (products) => {
