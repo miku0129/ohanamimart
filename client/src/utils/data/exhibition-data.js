b843ee643af3e7ae7da2560f02af92fffd778a36
miku0129 <svartkatt39@gmail.com>
2024-05-20 23:09:27 +0200

    docs: update exhibitions

--- client/src/utils/data/exhibition-data.js
+++ client/src/utils/data/exhibition-data.js
@@ -2,46 +2,31 @@ const EXHIBITIONS_DATA = {
   exhibitions: [
     {
       id: 0,
-      exhibition_title: "APRÈS-MIDI JAPONAIS",
-      start_date: "2024/3/9",
-      end_date: "2024/3/9",
-      start_time: "14:45",
-      end_time: "18:00",
-      location: "",
-      address: "",
-      exhibition_image_url: "https://i.ibb.co/kQBps9Y/28455.jpg",
-      exhibitors: [
-        "Nami Créations",
-      ],
-      about_exhibition:
-        "Cette édition 2024 de l'après-midi japonais vous permettra de (re)découvrir des aspects de la culture japonaise, en compagnie d'intervenants natifs japonais.",
-      exhibition_url: "https://www.helloasso.com/associations/association-franco-japonaise-du-vendomois/evenements/apres-midi-japonais-samedi-9-mars-2024-14h45-18h",
-    },
-    {
-      id: 1,
       exhibition_title: "MARCHÉ ARTISANAL JAPON, TOURS",
-      start_date: "2024/3/17",
-      end_date: "2024/3/17",
+      start_date: "2024/5/26",
+      end_date: "2024/5/26",
       start_time: "10:30",
-      end_time: "18:00",
+      end_time: "19:00",
       location: "YOÏSHO",
       address: "11, avenue Grammont Tours",
-      exhibition_image_url: "https://i.ibb.co/xhWvYWW/28340.jpg",
-      exhibitors: ["Tsubaki création", "Nami Créations", "GOEN"],
+      exhibition_image_url:
+        "https://live.staticflickr.com/65535/53732647335_5b6af48c0b_h.jpg",
+      exhibitors: ["Tsubaki création", "Nami Créations", "GOEN", "AT bijoux"],
       about_exhibition:
         "Créateurs japonais vendent des produits faits à la main",
       exhibition_url: "",
     },
     {
-      id: 2,
+      id: 1,
       exhibition_title: "MARCHÉ ARTISANAL JAPON, TOURS",
-      start_date: "2024/4/14",
-      end_date: "2024/4/14",
+      start_date: "2024/6/23",
+      end_date: "2024/6/23",
       start_time: "10:30",
-      end_time: "18:00",
+      end_time: "19:00",
       location: "YOÏSHO",
       address: "11, avenue Grammont Tours",
-      exhibition_image_url: "https://i.ibb.co/xhWvYWW/28340.jpg",
+      exhibition_image_url:
+        "https://live.staticflickr.com/65535/53732647335_5b6af48c0b_h.jpg",
       exhibitors: [
         "Tsubaki création",
         "Nami Créations",
@@ -52,7 +37,38 @@ const EXHIBITIONS_DATA = {
         "Créateurs japonais vendent des produits faits à la main",
       exhibition_url: "",
     },
-
+    {
+      id: 2,
+      exhibition_title: "MARCHÉ ARTISANAL JAPON, TOURS",
+      start_date: "2024/7/14",
+      end_date: "2024/7/14",
+      start_time: "10:30",
+      end_time: "19:00",
+      location: "YOÏSHO",
+      address: "11, avenue Grammont Tours",
+      exhibition_image_url:
+        "https://live.staticflickr.com/65535/53732647335_5b6af48c0b_h.jpg",
+      exhibitors: ["Tsubaki création", "Nami Créations", "GOEN"],
+      about_exhibition:
+        "Créateurs japonais vendent des produits faits à la main",
+      exhibition_url: "",
+    },
+    {
+      id: 3,
+      exhibition_title: "MARCHÉ ARTISANAL JAPON, TOURS",
+      start_date: "2024/7/27",
+      end_date: "2024/7/27",
+      start_time: "11:00",
+      end_time: "20:30",
+      location: "YOÏSHO",
+      address: "11, avenue Grammont Tours",
+      exhibition_image_url:
+        "https://live.staticflickr.com/65535/53732422478_a73a9d995d_h.jpg",
+      exhibitors: ["Tsubaki création", "Nami Créations", "Biscuits o riz"],
+      about_exhibition:
+        "Créateurs japonais vendent des produits faits à la main",
+      exhibition_url: "",
+    },
   ],
 };
 
